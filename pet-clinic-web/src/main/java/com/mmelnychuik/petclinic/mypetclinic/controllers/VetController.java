1b1343a708a32766861c960328f1bc25d309f3ee
marynamelnichuk <marynamelnichuk859@gmail.com>
2020-08-27 01:33:35 +0300

    Fixing version of petclinic-data jar in pom.xml

--- pet-clinic-web/src/main/java/com/mmelnychuik/petclinic/mypetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/mmelnychuik/petclinic/mypetclinic/controllers/VetController.java
@@ -5,12 +5,19 @@
 
 package com.mmelnychuik.petclinic.mypetclinic.controllers;
 
+import com.mmelnychuik.petclinic.mypetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
+
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
 
     @RequestMapping({"", "/index", "/index.html"})
     public String listVets(){
